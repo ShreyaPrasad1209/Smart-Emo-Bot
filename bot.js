c30c41f675f9ff87e477ea51ffe3763b936eeac7
Shreya Prasad <shreyapd06@gmail.com>
2019-10-01 16:18:56 +0530

    Add files via upload

--- bot.js
+++ bot.js
@@ -1,36 +1,30 @@
-const CognitiveServicesCredentials = require("@azure/ms-rest-js");
-const TextAnalyticsAPIClient = require("@azure/cognitiveservices-textanalytics");
-const express = require("express");
-let app = express();
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
 
-const {
-    ActivityHandler
-} = require('botbuilder');
+const { ActivityHandler } = require('botbuilder');
 
-class MyBot extends ActivityHandler {
+class EchoBot extends ActivityHandler {
     constructor() {
         super();
         // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
         this.onMessage(async (context, next) => {
-            let end = "https://westus.api.cognitive.microsoft.com/text/analytics/v2.1/sentiment?api_key=1d0b63768c434f9bbda8d148f3619aca";
-            // app.post('end?api_key=1d0b63768c434f9bbda8d148f3619aca', 
-            fetch(end, {
-                    method: 'POST',
-                    body: `${ context.activity.text }`
-                })
-                .then(res => res.json())
-
-
-            await context.sendActivity(res); //output response
+            let end = "https://westus.api.cognitive.microsoft.com/text/analytics/v2.1/sentiment?api_key=1d0b63768c434f9bbda8d148f3619aca"
+            fetch (end,{
+                method: 'POST',
+                body: `${ context.activity.text }`
+            })
+            .then(res => res.json())
+            .then(await context.sendActivity(res));
+    
             // By calling next() you ensure that the next BotHandler is run.
             await next();
         });
-        console.log('Heyy')
+
         this.onMembersAdded(async (context, next) => {
             const membersAdded = context.activity.membersAdded;
             for (let cnt = 0; cnt < membersAdded.length; ++cnt) {
                 if (membersAdded[cnt].id !== context.activity.recipient.id) {
-                    await context.sendActivity('Hello and welcome!');
+                    await context.sendActivity("Hey what's on your mind?");
                 }
             }
             // By calling next() you ensure that the next BotHandler is run.
@@ -39,6 +33,4 @@ class MyBot extends ActivityHandler {
     }
 }
 
-module.exports.MyBot = MyBot;
-console.log('Heyy')
-console.error();
+module.exports.EchoBot = EchoBot;
